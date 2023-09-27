7ed8321ab4c89a391a89042d087d335d71eca42d
itaitan <itaitan2@gmail.com>
2023-09-26 23:42:00 -0300

    global css

--- src/components/atoms/input/input.tsx
+++ src/components/atoms/input/input.tsx
@@ -1,3 +1,15 @@
-export const Input = (props: any) => {
-  return <input type="text" />
+import { InputHTMLAttributes } from 'react'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  width?: number
+  fullWidth?: boolean
+  label?: string
+  variant?: 'standard' | 'primary'
+  type?: 'text' | 'number' | 'email'
+  disabled?: boolean
+  required?: boolean
+}
+
+export const Input = (props: InputProps) => {
+  return <input {...props} className="border-2 border-gray-500 text-2xl" />
 }
